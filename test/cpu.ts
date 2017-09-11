98a58eb0415297aeefe402f71b3c17c12ffd4e20
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2017-09-11 19:36:17 +0300

    Test assigning and adding to registers

--- test/cpu.ts
+++ test/cpu.ts
@@ -1,15 +1,39 @@
 import CPU from "../src/cpu";
 import { expect } from "chai";
 
+/**
+ * Loads passed array of bytes to CPU and returns the CPU
+ * @param {Array<number>} bytes Array of bytes
+ * @return {CPU} Initialized CPU
+ */
+function initializeCpu(bytes: Array<number>): CPU {
+  const cpu = new CPU();
+  cpu.load(new Uint8Array(bytes));
+  return cpu;
+}
+
 describe("CPU", () => {
   it("should initialize memory at proper location", () => {
-    const cpu = new CPU();
-
     //Load instructions to clear screen and check that the instructions
     //were stored the start of the application memory area
-    cpu.load(new Uint8Array([0x00, 0xe0]));
-    expect(cpu.memory).to.not.equal(undefined);
+    const cpu = initializeCpu([0x00, 0xe0]);
+    cpu.next();
     expect(cpu.memory[0x200]).to.equal(0x00);
     expect(cpu.memory[0x201]).to.equal(0xe0);
+  });
+  it("should set register - 0x6XNN", () => {
+    //Check setting register VB to 0xFF
+    const cpu = initializeCpu([0x6b, 0xff]);
+    cpu.next();
+    expect(cpu.registers[0xb]).to.equal(0xff);
+  });
+  it("should add to register - 0x7XNN", () => {
+    const cpu = initializeCpu([0x7d, 0xff, 0x7d, 0x02]);
+    //Add 0xFF to empty register VD and check the register
+    cpu.next();
+    expect(cpu.registers[0xd]).to.equal(0xff);
+    //Add 0x2 to register VD and check that 8-bit overflow was handled
+    cpu.next();
+    expect(cpu.registers[0xd]).to.equal(0x1);
   });
 });
