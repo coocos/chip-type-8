f41ba35f720043baa1954f4202e92ea445b9fac4
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2017-09-26 20:43:56 +0300

    Add missing Node.js type definitions

--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,4 @@
+import "mocha";
 import { expect } from "chai";
 
 import * as utils from "../src/utils";
--- test/vm.ts
+++ test/vm.ts
@@ -1,3 +1,4 @@
+import "mocha";
 import { expect } from "chai";
 import * as sinon from "sinon";
 import * as _ from "lodash";
